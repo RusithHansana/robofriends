3af8b2362a0457fbc6cb287861bf5780d40cf229
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2023-11-26 14:31:08 +0530

    search function implemented

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from "react";
+import CardList from "./robo/CardList"
+import {robots} from "./robo/robots"
+import SearchBox from "./robo/SearchBox";
+
+//to use state we must declare class components
+
+export class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            robots: robots,
+            searchField: '',
+        }
+    }
+
+    onSearchChange = (event) =>{
+        this.setState({searchField: event.target.value});
+    }
+
+    render(){
+        const filteredRobots = this.state.robots.filter(robot =>{
+            return robot.name.toLowerCase()
+            .includes(this.state.searchField.toLowerCase());
+        });
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange = {this.onSearchChange}/>
+                <CardList robots = {filteredRobots}/>
+            </div>
+            
+        );
+    }
+}
+
+export default App;
