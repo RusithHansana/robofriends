37049ff87a7bbb38094b1056877800c04814c499
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2023-11-26 13:43:26 +0530

    dynamic card generation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './robo/Card';
+import CardList from './robo/CardList'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'; //css library similar to bootstrap
 import {robots} from './robo/robots';
 
-const robot = robots[0];
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card id = {robot.id} name = {robot.name} email = {robot.email}/> {/* This is props similar to passsing parameters to classes. We can
-                                                      use this props in Hello.js file*/}
+    <CardList robots = {robots}/>
   </React.StrictMode>
 );
 
