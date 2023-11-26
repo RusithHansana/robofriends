f971fe23bc8cf39fa6fdf1eefb7cc51024ca13c9
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2023-11-26 15:09:49 +0530

    added css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import CardList from "./robo/CardList"
 import {robots} from "./robo/robots"
 import SearchBox from "./robo/SearchBox";
+import './App.css';
 
 //to use state we must declare class components
 
@@ -17,15 +18,16 @@ export class App extends Component {
     onSearchChange = (event) =>{
         this.setState({searchField: event.target.value});
     }
-
+    
     render(){
         const filteredRobots = this.state.robots.filter(robot =>{
             return robot.name.toLowerCase()
             .includes(this.state.searchField.toLowerCase());
         });
+
         return (
             <div className="tc">
-                <h1>RoboFriends</h1>
+                <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange = {this.onSearchChange}/>
                 <CardList robots = {filteredRobots}/>
             </div>
--- src/robo/SearchBox.js
+++ src/robo/SearchBox.js
@@ -6,7 +6,7 @@ const SearchBox = ({searchChange}) => {
             <input 
                 className="pa3 ba b--green bg-lightest-blue"
                 type="search" 
-                placeholder="my robot friend"
+                placeholder="My robot friend"
                 onChange={searchChange}
             />
         </div>
